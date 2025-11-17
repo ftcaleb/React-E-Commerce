2103c2500c1724dd9bbf3f6627a59b8ed5693291
Ofentse Baloyi <tontochipi01@gmail.com>
2025-11-17 19:20:52 +0200

    Bag Section

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import Home from './Pages/Home'
-import Search  from './Components/Search'
-import Sidebar from './Components/Sidebar'
+import React from "react";
+import Home from "./Pages/Home";
+import Search from "./Components/Search";
+import Sidebar from "./Components/Sidebar";
+import Bag from "./Components/Bag";
 
 function App() {
   return (
-    <div className='flex bg-gray-200'>
-      <Sidebar/>
-      <div className='flex-1 justify-center p-4'>
-        <Search/>
-        <div className=''>
-        <Home/>
+    <div className="flex bg-gray-200">
+      <Sidebar />
+      <div className="flex-1 justify-center p-4">
+        <Search />
+        <div className="">
+          <Home />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/bag" element={<Bag />} />
+          </Routes>
         </div>
       </div>
-      
-    
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
